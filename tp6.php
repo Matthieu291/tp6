b572ef2193536ce4a4a2ded1537c5248fd67d1d6
Matthieu291 <matt.diard@gmail.com>
2020-03-19 11:54:18 +0100

    tp6

---
+++ tp6.php
@@ -0,0 +1,12 @@
+
+<?php
+echo "<h1>TP6</h1>";
+
+class team {
+    public $nom;
+    public $nbretitre;
+    function display(){
+        echo "L'équipe $this->nom a $this->nbretitre<br>";
+    }
+}
+?>
